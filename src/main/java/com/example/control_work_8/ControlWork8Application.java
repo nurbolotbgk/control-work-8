b9f749156b4525eb3f702a17c9648a6155bd31cc
Nurbolot Abilov <nurbolotbgk@gmail.com>
2026-08-29 12:59:54 +0600

    ftlh файлы restaurant_list.ftlh и restaurant_details.ftlh созданы

---
+++ src/main/java/com/example/control_work_8/ControlWork8Application.java
@@ -0,0 +1,13 @@
+package com.example.control_work_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlWork8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlWork8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/control_work_8/ControlWork8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control_work_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlWork8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
